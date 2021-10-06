03dbe9a9a8b7b568aaea294eee580754afdf1d7c
Gimna Katugampala <62742618+gimnakatugampala@users.noreply.github.com>
2021-10-06 13:47:04 -0700

    Update settings.py

--- YTdownloader/settings.py
+++ YTdownloader/settings.py
@@ -72,18 +72,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'YTdownloader.wsgi.application'
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
